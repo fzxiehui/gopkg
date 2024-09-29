8312cc968badc4befe3ac75c2d783982528f0b2a
fzxiehui <xie18960920183@outlook.com>
2024-09-29 14:29:03 +0800

    feat(uart): 分离接口

--- pkg/uart/uart.go
+++ pkg/uart/uart.go
@@ -9,36 +9,6 @@ import (
 	"go.bug.st/serial"
 )
 
-type Uart interface {
-
-	// 获取串口名
-	GetName() string
-
-	// 打开串口
-	Open() error
-
-	// 发送完成后返回，会阻塞
-	Send(buf []byte) (int, error)
-
-	// 读取, 会阻塞
-	Read() []byte
-
-	// 读取，会阻塞，可以设设置超时时间
-	ReadWithTimeout(timeout time.Duration) ([]byte, error)
-
-	// 带缓冲区的发送
-	SendWithBuffer(buf []byte)
-
-	// 关闭连接
-	Close()
-
-	// 内部函数: 发送协程
-	sendPoll()
-
-	// 内部函数: 接收协程
-	receivePoll()
-}
-
 type uart struct {
 	PortName       string      // 串口名字
 	BaudRate       int         // 通信波特率
